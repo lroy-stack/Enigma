a886cde4b7a9ed047b0c044cd9617f2406c99442
l.r0y <l.roy.lwe@gmail.com>
2024-05-29 02:45:32 +0200

    Update send_email.php

--- pages/send_email.php
+++ pages/send_email.php
@@ -1,11 +1,10 @@
 <?php
-// Mostrar errores de PHP para depuración
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recuperar y sanear los datos del formulario
     $nombre = htmlspecialchars($_POST['nombre']);
     $correo = htmlspecialchars($_POST['correo']);
     $fecha = htmlspecialchars($_POST['fecha']);
@@ -14,29 +13,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telefono = htmlspecialchars($_POST['telefono']);
     $mensaje = htmlspecialchars($_POST['mensaje']);
 
-    // Dirección de correo electrónico a la que se enviará el formulario
-    $to = "isa@enigmaconalma.com";
-    $subject = "Nueva reserva recibida";
+    $mail = new PHPMailer(true);
+    try {
+        // Configuración del servidor
+        $mail->isSMTP();
+        $mail->Host = 'smtp.hostinger.com'; // Cambia esto por el servidor SMTP de Hostinger
+        $mail->SMTPAuth = true;
+        $mail->Username = 'isa@enigmaconalma.com'; // Cambia esto por tu dirección de correo
+        $mail->Password = 'CocinaConAlma_20'; // Cambia esto por tu contraseña de correo
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587; // El puerto SMTP
 
-    // Crear el contenido del correo electrónico
-    $body = "Nombre: $nombre\n";
-    $body .= "Correo: $correo\n";
-    $body .= "Fecha: $fecha\n";
-    $body .= "Hora: $hora\n";
-    $body .= "Personas: $personas\n";
-    $body .= "Teléfono: $telefono\n";
-    $body .= "Mensaje: $mensaje\n";
+        // Remitente y destinatario
+        $mail->setFrom('', 'NUEVA RESERVA WEB'); // Cambia esto por tu dirección de correo y nombre
+        $mail->addAddress('isa@enigmaconalma.com', 'Enigma');
 
-    // Cabeceras del correo
-    $headers = "From: $correo" . "\r\n" .
-               "Reply-To: $correo" . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
+        // Contenido del correo
+        $mail->isHTML(true);
+        $mail->Subject = 'Nueva reserva recibida';
+        $mail->Body    = "Nombre: $nombre<br>Correo: $correo<br>Fecha: $fecha<br>Hora: $hora<br>Personas: $personas<br>Teléfono: $telefono<br>Mensaje: $mensaje";
 
-    // Enviar el correo
-    if (mail($to, $subject, $body, $headers)) {
-        echo "El mensaje se ha enviado correctamente.";
-    } else {
-        echo "Hubo un error al enviar el mensaje. Por favor, inténtelo de nuevo.";
+        $mail->send();
+        echo 'El mensaje se ha enviado correctamente.';
+    } catch (Exception $e) {
+        echo "Hubo un error al enviar el mensaje. Error de PHPMailer: {$mail->ErrorInfo}";
     }
 } else {
     echo "Método de solicitud no soportado.";
